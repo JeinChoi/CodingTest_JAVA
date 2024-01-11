9f06a651ed033b58358084f337d96ab37464b1d4
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-01-11 18:05:36 +0900

    [level Lv.0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/Lv.0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,16 +1,17 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] emergency) {
-       int[] answer = new int[emergency.length];
-        Integer[] arr = Arrays.stream(emergency).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr,Collections.reverseOrder());
-        int k=0;
-        for(int i=0;i<emergency.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(emergency[i]==arr[j])
-                    answer[k]=j+1;
+        int[] answer = new int[emergency.length];
+
+        for(int i = 0; i < answer.length; i++){
+          
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
+                }
             }
-            k++;
+            answer[i] = idx;
         }
         return answer;
     }
