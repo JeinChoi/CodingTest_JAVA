5bb809564cd20cd6a51a9abdaaef58833f4e7503
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-01-29 16:34:23 +0900

    [level 1] Title: 폰켓몬, Time: 4.43 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int num:nums) hm.put(num,hm.getOrDefault(num,0)+1);
+        if(hm.size()>(nums.length/2))
+         return nums.length/2;
+        return hm.size();  }
+}
