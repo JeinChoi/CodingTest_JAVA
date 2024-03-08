2612123f03f42ac568d69c0a7190a81765b7d962
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-03-08 15:40:13 +0900

    [level 1] Title: 과일 장수, Time: 0.49 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int m, int[] score) {
+       int answer = 0;int place=0;
+        Arrays.sort(score);
+        for(int i=score.length-1;i>=0;i-=m){
+             place = i-m+1;
+            if(place<0) break;
+            answer+=score[i-m+1]*m;
+
+        }
+        return answer;
+    }
+}
