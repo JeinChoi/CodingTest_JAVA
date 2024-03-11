1b235e9ad102cb82c3065051c38e323399beaa90
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-03-11 14:11:57 +0900

    [level 1] Title: 소수 찾기, Time: 148.34 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=2;i<=n;i++){
+           if(prime(i)==1)
+                answer++;
+        }
+        return answer;
+    }
+    
+    static public int prime(int num){
+        for(int i=2;i<=(int)Math.sqrt(num);i++){
+            if(num%i==0)
+                return 0;
+        }
+        return 1;
+    }
+}
