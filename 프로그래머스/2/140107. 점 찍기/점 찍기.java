96fb457c7cd4272e3bcc81dba7d474c605117035
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-03-13 20:45:32 +0900

    [level 2] Title: 점 찍기, Time: 0.07 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,14 +1,13 @@
 class Solution {
     public long solution(int k, int d) {
-       long count = 0;
-        //피타고라스 정리를 활용해서 풀이할 수 있다.
-        for(int i=0;i<=d;i+=k){
-                count+=(long)Math.sqrt(Math.pow(d,2)-Math.pow(i,2))/k+1;
-            //i가 x좌표라 했을 때 for문을 통해 이미 k씩 더해진다.
-            //k의 배수인 y 좌표만 구해야 하니까 k로 나눈다. 
-            //(x,0)도 구해야 하기 때문에 1을 더해준다.
-        }
-        
-        return count;
+       long answer = 0;
+         for(int x =0;x<=d;x+=k){
+             for(int y=0;y<=d;y+=k){
+                 if(Math.sqrt((long)Math.pow((x),2)+(long)Math.pow((y),2))<=d){
+                     answer++;
+                 }else break;
+             }
+         }
+         return answer;
     }
 }
