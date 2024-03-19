89bde028b4cf7af7a7f8ec8cf942c3986540554f
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-03-19 20:54:50 +0900

    [level 2] Title: 피보나치 수, Time: 3.47 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,20 +1,6 @@
 class Solution {
     public int solution(int n) {
-            int[] cal = new int[3];
-        cal[0]=0;cal[1]=1;cal[2]=1;
-        for(int i=3;i<=n;i++){
-            switch(i%3){
-                case 0:
-                    cal[0]=(cal[1]+cal[2])%1234567;
-                    break;
-                case 1:
-                    cal[1]=(cal[0]+cal[2])%1234567;
-                    break;
-                case 2:
-                    cal[2]=(cal[0]+cal[1])%1234567;
-                    break;
-            }
-        }
-        return cal[n%3]%1234567;
+        int answer = 0;
+        return answer;
     }
 }
