adf92c821874c5d34c18aaf189543c621be31dfc
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-04-06 15:35:49 +0900

    [Silver III] Title: 통계학, Time: 884 ms, Memory: 69644 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -6,7 +6,13 @@ import java.util.*;
 import static java.lang.Integer.MIN_VALUE;
 
 public class Main {
-
+/*
+* 최반값을 구하는 부분이 어려웠다.
+* 
+* 36%에서 계속 실패를 했는데 sum의 자료형을 float->double로 바꿨더니 해결이 됐다. 
+* 소수점 이하의 정밀도를 갖는 범위가 double이 커서 반올림을 했을 때 정확한 결과가 나온다 그래서 double로 바꿨더니 해결이 된것으로 예상된다.
+*
+* */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -38,42 +44,20 @@ public class Main {
     }
     static int getBestFrequency(int[] arr){
         Map<Integer,Integer> hm = new HashMap<>();
-        int max = MIN_VALUE;
+       
         for(int i=0;i<arr.length;i++){
             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
         }
 
         List<Integer> keys = new ArrayList<>(hm.keySet());
+        
         Collections.sort(keys);
-
-        int countSameNum=0;
-
-        Collections.sort(keys);
-//        for(int i=0;i<keys.size();i++){
-//            System.out.print(keys.get(i)+" ");
-//        }
-//        System.out.println();
         Collections.sort(keys,(v1,v2)->(hm.get(v2).compareTo(hm.get(v1))));
-
-//        for(int i=0;i<keys.size();i++){
-//            System.out.print(keys.get(i)+" ");
-//        }
-//        System.out.println();
-        //value값을 기준으로 내림차순 정렬 --> 빈도수가 높은 순으로 정렬
-//
-//        List<Integer> values = new ArrayList<>(hm.values());
-//        Collections.sort(values,Collections.reverseOrder());
-//        int temp=values.get(0);
-//        for(int i=1;i<values.size();i++){
-//                if(values.get(i)==temp)
-//                    countSameNum++;
-//                else break;
-//        }
-
-
+        
         if(keys.size()>1 && hm.get(keys.get(0))==hm.get(keys.get(1))){
             return keys.get(1);
         }
+        
         return keys.get(0);
     }
 }
