57908890280689ca5efd13e68102a2549a93c4e4
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-04-15 11:05:28 +0900

    [Silver III] Title: 주유소, Time: 412 ms, Memory: 36112 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+/*
+* 예상 논리
+* 1. 처음에는 무조건 첫번째 도시 가격에 맞춰서 기름을 사용해야 한다
+* 2. 거기서 추가로 첫번째 도시의 비용이 최소값이라면 다다음 경로 것까지 충전을 한다.
+* 3. 하나의 큐에는 도시 마다 리터 가격을 저장한다
+* 4. 다른 큐에는 도시 이동할 때마다 필요한 리터를 저장한다.
+* 5. 다음 기름 비용이 더 높다면 현재 기름 비용으로 다음 도시에 드는 비용까지 다 더해준다.
+* 6. 다음 기름 비용이 더 낮다면 최소 비용만 더해주고 continue한다.
+*
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+       Queue<Integer> costs = new LinkedList<>();
+       Queue<Integer> need = new LinkedList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            need.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st=new StringTokenizer(br.readLine());
+        while(st.hasMoreTokens()){
+            int input = Integer.parseInt(st.nextToken());
+            costs.add(input);
+        }
+
+        int result=0;
+        int nowCost = costs.poll();
+        while(!need.isEmpty()){
+
+            result+=nowCost*need.poll();
+            
+            if(nowCost>costs.peek()){
+                nowCost=costs.poll();
+            }
+            
+        }
+        System.out.println(result);
+    }
+
+}
