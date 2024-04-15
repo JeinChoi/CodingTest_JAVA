0aab058069ff101e4e7b7e4ff76bfc84835401fb
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-04-15 16:03:36 +0900

    [Gold V] Title: 택배 배송, Time: 464 ms, Memory: 40540 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -1,71 +1,70 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.lang.reflect.Array;
+import java.util.*;
 
+class Node{
+
+    public int to;
+    public int cows;
+
+
+    public Node(int to,int cows){
+        this.to=to;
+        this.cows=cows;
+    }
+
+
+}
+//to를 기준으로 큐에 저장한다
+//
 public class Main {
-
-    static int dist[];
-    static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    static int N,M;
+    static ArrayList<ArrayList<Node>> nodes = new ArrayList<>();
+    static int[] distance;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st =new StringTokenizer(br.readLine() );
-        N = Integer.parseInt(st.nextToken());   //헛간 그래프
-        M = Integer.parseInt(st.nextToken());   //간선
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int vertex = Integer.parseInt(st.nextToken());//정점
+        int edges = Integer.parseInt(st.nextToken());//간선
 
-        dist = new int[N+1];
+        distance=new int[vertex+1];
 
-        for (int i = 0; i <= N ; i++) {
-            graph.add(new ArrayList<>());
+        for(int i=0;i<=vertex;i++){
+            distance[i]=Integer.MAX_VALUE;
+            nodes.add(new ArrayList<>());
+        }
+        for(int i=0;i<edges;i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cows = Integer.parseInt(st.nextToken());
+
+            nodes.get(from).add(new Node(to, cows));
+            nodes.get(to).add(new Node(from,cows));
         }
 
-        for(int i =0;i<M;i++){
-            st= new StringTokenizer(br.readLine());
+        dijkstra(1);
+        System.out.println(distance[vertex]);
 
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            int C = Integer.parseInt(st.nextToken());
-
-            graph.get(A).add(new Node(B,C));
-            graph.get(B).add(new Node(A,C));
-        }
-
-        Arrays.fill(dist,Integer.MAX_VALUE);
-
-        dijst();
-        System.out.println(dist[N]);
     }
+    public static void dijkstra(int start){
+    PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2)->o1.cows-o2.cows);
+    distance[start]=0;
+    pq.offer(new Node(start,0));
 
-    static void dijst(){
-        PriorityQueue<Node> queue = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
-        queue.add(new Node(1,0));
-        dist[1] = 0;
+    while(!pq.isEmpty()){
+        Node now = pq.poll();
 
-        while (!queue.isEmpty()){
-              Node now = queue.poll();
+        for(Node node : nodes.get(now.to)){
+            if(distance[now.to]+node.cows<distance[node.to]){
+                distance[node.to] = distance[now.to]+node.cows;
+                 pq.offer(new Node(node.to,distance[node.to]));
 
-              for(Node next : graph.get(now.v)){
-                  if(dist[next.v] > dist[now.v]+next.cost){
-                      dist[next.v] =dist[now.v]+next.cost;
-                      queue.add(new Node(next.v,dist[next.v]));
-                  }
-              }
+            }
         }
     }
-
-    static class Node{
-        int v;
-        int cost;
-
-        public Node(int v,int cost){
-                this.v = v;
-                this.cost = cost;
-        }
     }
 }
