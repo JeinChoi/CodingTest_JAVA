a7705268890fcd4193391e6aff70cd66cc2ab507
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-11-11 15:27:46 +0900

    [Bronze IV] Title: 주사위 세개, Time: 104 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,24 +1,24 @@
-import java.util.*;
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer[] arr= Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).boxed().toArray(Integer[]::new);
 
-    public static void main(String[] args){
-       Scanner sc = new Scanner(System.in);
-        int[] arr = new int[3];
-        for(int i=0;i<3;i++){
-          arr[i]=sc.nextInt();
+        if (arr[0]==arr[1] && arr[1]==arr[2]) {
+            System.out.println(10000+ arr[0]*1000);
         }
-        if(arr[0]==arr[1] && arr[0]==arr[2]){
-            System.out.println(10000+arr[0]*1000);
+        else if((arr[0]!=arr[1] && arr[0]==arr[2] )
+                || (arr[0]!=arr[2] && arr[0]==arr[1])){
+            System.out.println(1000+ arr[0]*100);
         }
-        else if(arr[0]==arr[1]&&arr[0]!=arr[2] || arr[0]==arr[2]&&arr[0]!=arr[1]){
-            System.out.println(1000+arr[0]*100);
-        }
-        else if(arr[1]==arr[2] && arr[1]!=arr[0]){
-            System.out.println(1000+arr[1]*100);
+        else if(arr[1]==arr[2]&&arr[1]!=arr[0]){
+            System.out.println(1000+ arr[1]*100);
         }
         else{
-            System.out.println(Math.max(arr[0],Math.max(arr[1],arr[2]))*100);
+            System.out.println(Math.max(Math.max(arr[0],arr[1]),arr[2])*100);
         }
-
     }
 }
