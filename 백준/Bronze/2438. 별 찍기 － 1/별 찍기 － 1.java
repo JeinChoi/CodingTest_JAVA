533fa3d71dea8266a1bc16342c3cea5df9de202a
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-11-11 14:40:17 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,14 +1,19 @@
-import java.util.*;
-public class Main{
-     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println("");
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i=1;i<=num;i++){
+            sb.append("*".repeat(Math.max(0, i)));
+            System.out.println(sb.toString());
+            sb.setLength(0);
         }
     }
-
-}
+   }
