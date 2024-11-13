7c73ee534569933d87904aee58715ca4b51bc185
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-11-13 22:42:45 +0900

    [Bronze II] Title: 문자열 반복, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,16 +1,28 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-public class Main{ 
-public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
-            int mul=sc.nextInt();
-            String str = sc.next();
-            StringBuilder sb = new StringBuilder();
-            for(int j=0;j<str.length();j++){
-                sb.append((str.charAt(j)+"").repeat(mul));
+            String[] strArr = br.readLine().split(" ");
+            
+            int m = Integer.parseInt(strArr[0]);
+            int strLength = strArr[1].length();
+
+            for(int j=0;j<strLength;j++){
+               sb.append((strArr[1].charAt(j)+"").repeat(m));
             }
-            System.out.println(sb.toString());
+            System.out.println(sb);
+            sb.setLength(0);
         }
 
     }
