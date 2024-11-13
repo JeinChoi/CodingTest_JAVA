558f25898cc773c2f6d0acdc75373c04b1a747e2
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-11-13 22:57:51 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 116 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -1,27 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+class Main {
 
-public class Main {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String star="*";
-        int n=sc.nextInt();
-        int j=n-1;
-        for(int i=1;i<n;i++){
-            System.out.print(star.repeat(i));
-            System.out.print(" ".repeat(j*2));
-            System.out.println(star.repeat(i));
-            j--;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int total = n*2;
+        for(int i=0;i<n;i++){
+                int star = i+1;
+                sb.append("*".repeat(star));
+                sb.append(" ".repeat(total-star*2));
+                sb.append("*".repeat(star));
+
+                System.out.println(sb);
+                sb.setLength(0);
         }
-        System.out.println(star.repeat(n*2));
-        j=1;
-        for(int i=n-1;i>=0;i--){
-            System.out.print(star.repeat(i));
-            System.out.print(" ".repeat(j*2));
-            System.out.println(star.repeat(i));
-            j++;
-        }
+        for(int i=n-1;i>0;i--){
+            int star=i;
+            sb.append("*".repeat(star));
+            sb.append(" ".repeat(total-star*2));
+            sb.append("*".repeat(star));
 
+            System.out.println(sb);
+            sb.setLength(0);
+
+        }
     }
 }
