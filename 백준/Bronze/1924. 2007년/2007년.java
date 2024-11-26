5f0395c2e231b8a2a0a940d37b550a5fc74a9d7b
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-11-26 21:09:29 +0900

    [Bronze I] Title: 2007년, Time: 104 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,15 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-public class Main {
+import java.lang.*;
+class Main {
 
-    public static void main(String[] args) {
-        int[] month={31,28,31,30,31,30,31,31,30,31,30,31};
-        String[] week={"MON","TUE","WED","THU","FRI","SAT","SUN"};
-        Scanner sc = new Scanner(System.in);
-        int x=sc.nextInt();int y=sc.nextInt();
-        int days=0;
-        for(int i=0;i<x-1;i++){
-            days+=month[i];
-        }days+=y-1;        
-        System.out.println(week[days%7]);
+    public static void main(String[] args) throws IOException {
+
+        int[] days= {31,28,31,30,31,30,31,31,30,31,30,31};
+        String[] week = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[2];
+        int i=0; int total=0;
+        while(st.hasMoreTokens()){
+            arr[i]=Integer.parseInt(st.nextToken());
+            i++;
+        }
+        for(int j=0;j<arr[0]-1;j++){
+            total+=days[j];
+        }
+        total+=arr[1];
+        System.out.println(week[total%7]);
+
     }
 }
