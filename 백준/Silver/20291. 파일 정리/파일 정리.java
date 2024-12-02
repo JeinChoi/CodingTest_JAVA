34264f5dfe34e22af6eb886d6b9332a9b0b5c906
JeinChoi <59508337+JeinChoi@users.noreply.github.com>
2024-12-02 22:09:31 +0900

    [Silver III] Title: 파일 정리, Time: 1196 ms, Memory: 61844 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,19 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.*;
 import java.util.*;
 
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        Map<String,Integer> map = new HashMap<>();
 
-public class Main {
+        for(int i=0;i<num;i++){
+            String[] str = br.readLine().split("\\.");
+            map.put(str[1],map.getOrDefault(str[1],0)+1);
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-       int num = sc.nextInt();
-       Map<String,Integer> hm = new TreeMap<>();
-       for(int i=0;i<num;i++){
-           String[] arr = sc.next().split("\\.");
-           hm.put(arr[1],hm.getOrDefault(arr[1],0)+1);
-       }
+        }
+        List<String> ll = new LinkedList<>(map.keySet());
+        Collections.sort(ll);
 
-       for(String key : hm.keySet()){
-           System.out.println(key+" "+hm.get(key));
-       }
+        for(String key : ll){
+            System.out.println(key+" "+map.get(key));
+        }
     }
 }
